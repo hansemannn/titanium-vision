f3a2d546f84c6a3ff195a4cf026e76463f17bf66
Hans Knöchel <h.knoechel@lambus.com>
2023-04-25 21:03:33 +0200

    feat: add new APIs

--- Classes/TiVisionModule.m
+++ Classes/TiVisionModule.m
@@ -247,6 +247,32 @@
     UIImage *inputImage = nil;
     NSError *requestHandlerError = nil;
 
+    NSInteger maximumObservations = [TiUtils intValue:@"maximumObservations" properties:args def:1];
+    CGFloat minimumAspectRatio = [TiUtils floatValue:@"minimumAspectRatio" properties:args def:0.5f];
+    CGFloat maximumAspectRatio = [TiUtils floatValue:@"maximumAspectRatio" properties:args def:1.0f];
+    CGFloat quadratureTolerance = [TiUtils floatValue:@"quadratureTolerance" properties:args def:30.0f];
+    CGFloat minimumSize = [TiUtils floatValue:@"minimumSize" properties:args def:0.2f];
+  
+    if (maximumObservations < 0) {
+      [self throwException:@"Invalid bounds provided for \"maximumObservations\"" subreason:@"Please pass a value greater or equal to 0" location:CODELOCATION];
+    }
+
+    if (minimumAspectRatio < 0 || minimumAspectRatio > 1) {
+      [self throwException:@"Invalid bounds provided for \"minimumAspectRatio\"" subreason:@"Please pass a value between 0.0 and 1.0" location:CODELOCATION];
+    }
+  
+    if (maximumAspectRatio < 0 || maximumAspectRatio > 1) {
+      [self throwException:@"Invalid bounds provided for \"maximumAspectRatio\"" subreason:@"Please pass a value between 0.0 and 1.0" location:CODELOCATION];
+    }
+
+    if (quadratureTolerance < 0 || quadratureTolerance > 45) {
+      [self throwException:@"Invalid bounds provided for \"quadratureTolerance\"" subreason:@"Please pass a value between 0 and 45" location:CODELOCATION];
+    }
+  
+    if (minimumSize < 0) {
+      [self throwException:@"Invalid bounds provided for \"minimumSize\"" subreason:@"Please pass a value between 0.0 and 1.0" location:CODELOCATION];
+    }
+
     if ([image isKindOfClass:[NSString class]] || [image isKindOfClass:[TiBlob class]]) {
         inputImage = [TiUtils image:[args objectForKey:@"image"] proxy:self];
     } else {
@@ -279,6 +305,12 @@
 
         [callback call:@[event] thisObject:self];
     }];
+  
+    request.minimumAspectRatio = minimumAspectRatio;
+    request.maximumAspectRatio = maximumAspectRatio;
+    request.quadratureTolerance = quadratureTolerance;
+    request.minimumSize = minimumSize;
+    request.maximumObservations = maximumObservations;
 
     VNImageRequestHandler *handler = [[VNImageRequestHandler alloc] initWithCGImage:inputImage.CGImage options:@{}];
     [handler performRequests:@[request] error:&requestHandlerError];
